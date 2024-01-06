850793499a70750189309de2ed1d88703313cfa4
Suratt028 <154447222+Suratt028@users.noreply.github.com>
2024-01-06 21:50:35 +0700

    Update ARREY.java

--- ARREY.java
+++ ARREY.java
@@ -1,160 +1,108 @@
-package Array;
-
-
+import java.util.Arrays;
 import java.util.Scanner;
 
+public class ArrayProgram {
 
-public class Eightmethod {
-    public static void nprint(int x[]) {
-        for (int i = 0; i < x.length; i++) {
-            System.out.print(x[i] + " ");
+    public static void insertElements(int[] arr) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter elements of the array: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
         }
     }
 
-
-    public static void insertValue(int x[]) { // Corrected method name
-        Scanner scan = new Scanner(System.in);
-        for (int i = 0; i < x.length; i++) {
-            System.out.print("Enter value for element " + (i + 1) + ": ");
-            x[i] = scan.nextInt();
-        }
+    public static void printArray(int[] arr) {
+        System.out.println(Arrays.toString(arr));
     }
 
-
-    public static int sumArray(int x[]) {
+    public static int calculateSum(int[] arr) {
         int sum = 0;
-        for (int i = 0; i < x.length; i++) {
-            sum += x[i];
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
         }
         return sum;
     }
 
-
-    public static double calculateAverage(int x[]) {
-        int sum = 0;
-        for (int i = 0; i < x.length; i++) {
-            sum += x[i];
-        }
-        return (double) sum / x.length;
+    public static double calculateAverage(int[] arr) {
+        return (double) calculateSum(arr) / arr.length;
     }
 
-
-    public static int Max(int[] array) {
-        if (array.length == 0) {
-            throw new IllegalArgumentException("Array is empty");
-        }
-
-
-        int max = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] > max) {
-                max = array[i];
+    public static int findMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
             }
         }
         return max;
     }
 
-
-    public static int Min(int[] array) {
-        if (array.length == 0) {
-            throw new IllegalArgumentException("Array is empty");
-        }
-
-
-        int min = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] < min) {
-                min = array[i];
+    public static int findMin(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
             }
         }
         return min;
     }
 
-
-    public static int SecondMax(int[] array) {
-        if (array.length < 2) {
-            throw new IllegalArgumentException("Array should have at least two elements");
-        }
-
-
+    public static int findSecondMax(int[] arr) {
         int max = Integer.MIN_VALUE;
         int secondMax = Integer.MIN_VALUE;
-
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > max) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
                 secondMax = max;
-                max = array[i];
-            } else if (array[i] > secondMax && array[i] != max) {
-                secondMax = array[i];
+                max = arr[i];
+            } else if (arr[i] > secondMax && arr[i] != max) {
+                secondMax = arr[i];
             }
         }
-
-
         if (secondMax == Integer.MIN_VALUE) {
             throw new IllegalArgumentException("No second maximum element found");
         }
-
-
         return secondMax;
     }
 
-
-    public static int SecondMin(int[] array) {
-        if (array.length < 2) {
-            throw new IllegalArgumentException("Array should have at least two elements");
-        }
-
-
+    public static int findSecondMin(int[] arr) {
         int min = Integer.MAX_VALUE;
         int secondMin = Integer.MAX_VALUE;
-
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < min) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
                 secondMin = min;
-                min = array[i];
-            } else if (array[i] < secondMin && array[i] != min) {
-                secondMin = array[i];
+                min = arr[i];
+            } else if (arr[i] < secondMin && arr[i] != min) {
+                secondMin = arr[i];
             }
         }
-
-
         if (secondMin == Integer.MAX_VALUE) {
             throw new IllegalArgumentException("No second minimum element found");
         }
-
-
         return secondMin;
     }
 
-
-    public static void asdSorting(int A1[]) {
-        int n = A1.length;
+    public static void sortAscending(int[] arr) {
+        int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
-            for (int x = 0; x < n - i - 1; x++) {
-                if (A1[x] > A1[x + 1]) {
-                    int temp = A1[x];
-                    A1[x] = A1[x + 1];
-                    A1[x + 1] = temp;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
     }
 
-
-    public static void deSorting(int A1[]) {
-        int n = A1.length;
+    public static void sortDescending(int[] arr) {
+        int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
-                if (A1[j] < A1[j + 1]) {
-                    int temp = A1[j];
-                    A1[j] = A1[j + 1];
-                    A1[j + 1] = temp;
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
                 }
             }
-
-
         }
     }
 
