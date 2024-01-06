c8a1d22a4be084ae36537777647fbe150173692d
Suratt028 <154447222+Suratt028@users.noreply.github.com>
2024-01-06 21:44:26 +0700

    Update ARREY.java

--- ARREY.java
+++ ARREY.java
@@ -1,10 +1,10 @@
-package Class;
+package Array;
 
 
 import java.util.Scanner;
 
 
-public class Class {
+public class Eightmethod {
     public static void nprint(int x[]) {
         for (int i = 0; i < x.length; i++) {
             System.out.print(x[i] + " ");
